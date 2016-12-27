25de7409dc83c3b219ee398ca6d4951e349db8e9
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-12-26 23:44:20 -0600

    operador spread

--- lecciones/operadores_rest_spread/main.js
+++ lecciones/operadores_rest_spread/main.js
@@ -1,5 +1,18 @@
+// El operador rest (...) toma todos parátros y los guarda en un array
+
 function sumar(...numeros) {
     return numbers.reduce(function(acc, item){
-        return acc, item;
+        return acc + item;
     })
 }
+
+
+//  El operador spread toma un array y manda cada elemento de este como
+// argumentos para una función
+
+function multiplicar(x, y){
+    return x * y;
+}
+
+numeros = [3, 5];
+console.log(...numeros); // manda  a llamar la función con un parámetro por cada elemento del array
