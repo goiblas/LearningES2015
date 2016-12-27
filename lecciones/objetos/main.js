a074898005ce0f4ed75d395ddb2048f21e6afd37
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-12-26 23:50:25 -0600

    template strings

---
+++ lecciones/objetos/main.js
@@ -0,0 +1,12 @@
+let nombre = "Héctor";
+let edad = 27;
+
+function persona(nombre, edad){
+    return {
+        name,
+        edad,
+        saludar(){
+            return `Hola, ${this.name}`;
+        }
+    }
+}
