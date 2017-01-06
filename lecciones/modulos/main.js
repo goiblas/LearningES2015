d9407b809c11173d43757a611c7fd06068d58256
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-01-05 23:59:30 -0600

    Ejemplo de método en clase

--- lecciones/modulos/main.js
+++ lecciones/modulos/main.js
@@ -1,8 +1,13 @@
 export something = "algo"
 
 // export can be used several times
-export class aClass{
+export class ThisIsAClass{
     constructor(msj){
         console.log(msj);
+    }
+
+    estoEsUnMetodo(param1, param2){
+        console.log(param1);
+        console.log(param2);
     }
 }
